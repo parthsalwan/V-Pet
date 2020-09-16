5c0a09847913315c6432d52c1a9526dab43d065e
parthsalwan <66077080+parthsalwan@users.noreply.github.com>
2020-09-17 00:06:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,54 @@
 //Create variables here
 var dog
-var happyDog, database, foodS, foodStock
+var dogHappy, database, foodS, foodStock
 
 function preload()
 {
   //load images here
   dog=loadImage("images/dogImg.png")
-
+dogHappy=loadImage("images/dogImg1.png")
 }
 
 function setup() {
 	createCanvas(800, 700);
-  database=firebase.database();
+  database=firebase.database()
+
+foodStock=database.ref('food');
+foodStock.on("value",readStock);
 
 }
 
 
 function draw() {  
 
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS);
+dog.addImage(dogHappy);
 
+}
   drawSprites();
   //add styles here
 
 }
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+
+if(x<=0){
+  x=0
+}else{
+  x=x-1;
+}
+
+  database.ref('/').update({
+
+Food:x
+})
+}
+
 
 
 
